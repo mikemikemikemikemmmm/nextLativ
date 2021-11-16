b0752183b62309a7344015aa4afa1479823aca5e
mike yang <@gmail.com>
2021-11-16 14:58:03 +0800

    fix error

--- src/api/getStaticProps.ts
+++ src/api/getStaticProps.ts
@@ -9,10 +9,10 @@ export const getStaticPropApi = {
     getAsideByCategoryRoute: async(category_route: string) => {
         return (await HttpClient.get<TAside>(`/getStaticProps/asideByCategoryRoute/${category_route}`)).data
     },
-    getSeriesBySubStyleRoute:async (category_route, style_route, sub_style_route) => {
+    getSeriesBySubStyleRoute:async (category_route:string, style_route:string, sub_style_route:string) => {
         return (await HttpClient.get<ISeries[]>(`/getStaticProps/seriesByStyleRoute/${category_route}/${style_route}/${sub_style_route}`)).data
     },
-    getSeriesByCategoryRoute:async (category_route) => {
+    getSeriesByCategoryRoute:async (category_route:string) => {
         return (await HttpClient.get<ISeries[]>(`/getStaticProps/seriesByCategory/${category_route}`)).data
     }
 }
