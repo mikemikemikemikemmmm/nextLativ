3fdc88170186aabf6fd6b57763be63f6be5cbdd5
mike yang <@gmail.com>
2021-11-17 13:51:52 +0800

    附註混合內容警語

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
     return (
         <div style={{ height: '75vh' }} className="w-full flex items-center justify-center">
             <h1 className='text-7xl'>Lativ 模仿</h1>
+            <h1 className='text-7xl'>因前端部署在https、後端在http，會造成混合內容，請關閉瀏覽器的強制https功能</h1>
         </div>
 
     )
